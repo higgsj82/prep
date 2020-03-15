bacaf3bde514bedf19c230a293e97756d42f67e1
jhiggs <higgsj82@gmail.com>
2020-03-15 11:36:55 -0400

    longest palindrome

--- ruby.rb
+++ ruby.rb
@@ -101,3 +101,68 @@ def productify(arr)
 
   products
 end
+
+# longest_palindrome
+# Write a function that will take a string and return the indices of the start/end of the longest palindrome it contains.
+
+# Example:
+# longest_palindrome('acapella') => [0, 2]
+
+# my solution
+def isPalindrome?(str)
+  last = str.length - 1
+
+  (0..(last/2)).each do |i|
+    return false if str[i] != str[last - i]
+  end
+  return true
+end
+
+
+def substrings(str)
+  arr = []
+  (0...str.length).each do |i|
+    (i...str.length).each do |j|
+      arr << str[i..j]
+    end
+  end
+  arr.sort_by(&:length).reverse
+end
+
+def longest_palindrome(str)
+  subs = substrings(str) # ordered array from largest to smallest
+  subs.map{|sub| return sub if isPalindrome?(sub) && sub.length != 1}
+  return false
+end
+
+# aA solution
+
+def longest_palindrome(string)
+  best_palindrome_start = 0
+  best_palindrome_len = 0
+
+  0.upto(string.length - 1).each do |start|
+    # micro-optimization: don't look at substrings shorter than best
+    # palindrome.
+    len = best_palindrome_len + 1
+    while start + len <= string.length
+      if is_palindrome?(string, start, len)
+        best_palindrome_start, best_palindrome_len = start, len
+      end
+
+      len += 1
+    end
+  end
+
+  [best_palindrome_start, best_palindrome_start + best_palindrome_len - 1]
+end
+
+def is_palindrome?(string, start, len)
+  len.times do |i|
+    if string[start + i] != string[(start + len - 1) - i]
+      return false
+    end
+  end
+
+  true
+end
