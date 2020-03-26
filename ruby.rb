e9086876daf1d86ffd64ff9490d4b998f3f26dbe
jhiggs <higgsj82@gmail.com>
2020-03-25 23:33:40 -0400

    look and say

--- ruby.rb
+++ ruby.rb
@@ -344,4 +344,39 @@ def move_zeros2(arr)
   arr
 end
 
+look_and_say
+Implement the 'look and say' function. 'Look and say' takes an input array and outputs an array that describes the count of the elements in the input array as they appear in order.
 
+Example:
+
+# there is one '1' in the input array
+look_and_say([1]) == [[1, 1]]
+
+# there are two '1's in the input array
+look_and_say([1, 1]) == [[2, 1]]
+
+# there is one '2', followed by one '1' in the input array
+look_and_say([2, 1]) == [[1, 2], [1, 1]]
+
+# is one '1', followed by one '2', followed by 2 '1's in the input
+# array
+look_and_say([1, 2, 1, 1]) == [[1, 1], [1, 2], [2, 1]]
+Solution
+Maintain a current count, maintain a current element. Push both onto new array when a different element is detected.
+
+def look_and_say(array)
+  return [] if array.empty?
+
+  output = [[1, array[0]]]
+
+  (1...array.length).each do |idx|
+    el = array[idx]
+    if el == output.last[1]
+      output.last[0] += 1
+    else
+      output << [1, el]
+    end
+  end
+
+  output
+end
