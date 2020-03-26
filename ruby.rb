770b6589fba0677ba6a07a0cf34afd2375933eb9
jhiggs <higgsj82@gmail.com>
2020-03-25 23:36:30 -0400

    stack queue

--- ruby.rb
+++ ruby.rb
@@ -384,3 +384,27 @@ def look_and_say(array)
 
   output
 end
+
+StackQueue
+Implement a queue using stacks. That is, write enqueue and dequeue using only push and pop operations.
+
+In terms of performance, enqueue should be O(1), but dequeue may be worst-case O(n). In terms of ammortized time, dequeue should be O(1). Prove that your solution accomplishes this.
+
+Solution
+class StackQueue
+  def initialize
+    @in, @out = [], []
+  end
+
+  def enqueue(value)
+    @in << value
+  end
+
+  def dequeue
+    if @out.empty?
+      @out << @in.pop until @in.empty?
+    end
+
+    @out.pop
+  end
+end
