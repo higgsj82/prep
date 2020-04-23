a151d306598358f9c67f9fc1671de645d165647b
jhiggs <higgsj82@gmail.com>
2020-04-22 23:18:10 -0400

    inner loop

--- js.js
+++ js.js
@@ -1,7 +1,8 @@
 // Two Number Sum
 function twoNumberSum(array, targetSum) {
     for (let i = 0; i < array.length; i++) {
-        
+        for (let j = i + 1; j <= array.length; j++) {
+        }
     }
 }
 
