0574cdc88147d98b3b7066186af55250f903f736
jhiggs <higgsj82@gmail.com>
2020-04-22 23:14:22 -0400

    for loop with three pointers

--- js.js
+++ js.js
@@ -1,1 +1,11 @@
 // Three Number Sum
+
+function threeNumberSum(array, targetSum) {
+    let sortedArr = array.sort();
+
+    for (let i = 1; i < sortedArr.length; i++) {
+        let mid = sortedArr[i];
+        let left = sortedArr[i - 1];
+        let right = sortedArr[i + 1];
+	}
+}
